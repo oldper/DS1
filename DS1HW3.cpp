7d3d430e8eb786a427251a209bf09653b3b47c8e
oldpercent <oldpercent@gmail.com>
2022-10-19 15:21:09 +0800

    init HW3

--- DS1HW3.cpp
+++ DS1HW3.cpp
@@ -1,4 +1,45 @@
 # include <iostream>
 # include <string>
 using namespace std;
+	
+bool cmdCorrect( string str ) {
+	if ( str == "0" || str == "1" ) return true;
+	cout << "Error!!!!!!!" << endl;
+	return false;
+} // check if the cmd correct.
 
+string removeSpace( string str ) {
+	int len = str.length();
+	for ( int i = 0 ; i < len ; i++ ) {
+		if ( str[i] == ' ' ) str.erase( str.begin() + i );
+	}
+	return str;
+}
+
+string readCmd() {
+	string cmd;
+	do {
+		cin >> cmd;
+	} while ( !cmdCorrect( cmd ) );
+	return cmd;
+} // readCMD
+
+int main() {
+	cout << "-------------------------------" << endl;
+	cout << "1. start" << endl;
+	cout << "0. quit" << endl;
+	cout << "-------------------------------" << endl;
+	string cmd = readCmd();
+	cin.get();
+
+	while ( cmd != "0" ) { 
+		string str;
+		cout << "Input infix natation" << endl;	
+		getline(cin,str);
+		str = removeSpace( str );
+		cout << str << endl;
+		cmd = readCmd();
+		cin.get();
+	} //while
+	  //
+} // main()
